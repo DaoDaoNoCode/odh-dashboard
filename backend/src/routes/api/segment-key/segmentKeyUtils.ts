720a20b985130a32cfcd48983444034dcea4045d
Juntao Wang <juntwang@redhat.com>
2021-05-28 19:41:56 -0400

    test

--- backend/src/routes/api/segment-key/segmentKeyUtils.ts
+++ backend/src/routes/api/segment-key/segmentKeyUtils.ts
@@ -5,9 +5,6 @@ export const getSegmentKey = async (
 ): Promise<ODHSegmentKey> => {
   const coreV1Api = fastify.kube.coreV1Api;
   const namespace = fastify.kube.namespace;
-  const customObjectsApi = fastify.kube.customObjectsApi;
-  const clusterVersionList = await customObjectsApi.listClusterCustomObject('config.openshift.io', 'v1', 'clusterversions')
-  console.log('cluster id: ' + (clusterVersionList.body as any).items[0]?.spec.clusterID);
   try {
     const res = await coreV1Api.readNamespacedSecret('rhods-segment-key', namespace);
     const decodedSegmentKey = Buffer.from(res.body.data.segmentKey, 'base64').toString();
